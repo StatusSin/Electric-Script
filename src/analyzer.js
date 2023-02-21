10386cbe477ead0165fb54c681ab916e8dd3972c
Jurgis Stepenka <jstepenka@gmail.com>
2023-02-21 11:10:59 -0800

    updating analyzer

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,3 +1,11 @@
+import fs from "fs";
+import ohm from "ohm-js";
+import * as core from "./core.js";
+
+const electricScriptGrammar = ohm.grammar(
+  fs.readFileSync("src/electricScript.ohm")
+);
+
 export default function analyze(sourceCode) {
   const match = electricScriptGrammar.match(sourceCode);
   if (!match.succeeded()) error(match.message);
