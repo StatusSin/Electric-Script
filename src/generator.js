b9a118ad0b95dc442103613289ef36cf9c65aff7
gsonkow <sonkowskyg@gmail.com>
2023-05-03 00:13:12 -0700

    removed some notes

--- src/generator.js
+++ src/generator.js
@@ -1,28 +1,5 @@
 import { IfStatement } from "./core.js";
 
-// may need to fix analyzer and core to make this work i think
-// pretty sure core is missing some features (check commented out code and carlos/src/core.js)
-// some i found:
-// -break statement
-// -boolean
-// -conditional
-// -variable (not just declaration)
-// -while
-// -field (?)
-// -functionDeclaration (?)
-// -function call
-
-// also these but i think we can live without them:
-// -super
-// -this (?)
-// -new
-// -extends
-
-// not sure if we plan to implement type declaration
-// commented out the version from carlos as an example, not sure if its useful
-
-// not sure how our arrays work (right now it seems pretty different from carlos) need some help with that
-
 export default function generate(program) {
   const output = [];
 
@@ -48,26 +25,6 @@ export default function generate(program) {
     VarDeclaration(d) {
       output.push(`let ${gen(d.variable)} = ${gen(d.initializer)};`);
     },
-    // TypeDeclaration(d) {
-    //   // The only type declaration in Carlos is the struct! Becomes a JS class.
-    //   output.push(`class ${gen(d.type)} {`);
-    //   output.push(`constructor(${gen(d.type.fields).join(",")}) {`);
-    //   for (let field of d.type.fields) {
-    //     output.push(`this[${JSON.stringify(gen(field))}] = ${gen(field)};`);
-    //   }
-    //   output.push("}");
-    //   output.push("}");
-    // },
-
-    // Field(f) {
-    //   return targetName(f);
-    // },
-
-    // FunctionDeclaration(d) {
-    //   output.push(`function ${gen(d.fun)}(${gen(d.params).join(", ")}) {`);
-    //   gen(d.body);
-    //   output.push("}");
-    // },
     Variable(v) {
       // Standard library constants just get special treatment
       return targetName(v);
@@ -79,17 +36,9 @@ export default function generate(program) {
       output.push(`${gen(s.target)} = ${gen(s.source)};`);
     },
 
-    // BreakStatement(s) {
-    //   output.push("break;");
-    // },
-
     Return(s) {
       output.push(`return ${gen(s.expression)};`);
     },
-
-    // ShortReturnStatement(s) {
-    //   output.push("return;");
-    // },
 
     IfStatement(s) {
       output.push(`if (${gen(s.test)}) {`);
@@ -103,33 +52,11 @@ export default function generate(program) {
         output.push("}");
       }
     },
-
-    // WhileStatement(s) {
-    //   output.push(`while (${gen(s.test)}) {`);
-    //   gen(s.body);
-    //   output.push("}");
-    // },
-
-    For(s) {
-      //TODO: i can do this later
-      // const i = targetName(s.iterator);
-      // const op = s.op === "..." ? "<=" : "<";
-      // output.push(
-      //   `for (let ${i} = ${gen(s.low)}; ${i} ${op} ${gen(s.high)}; ${i}++) {`
-      // );
-      // gen(s.body);
-      // output.push("}");
-    },
     ForArg(s) {
       output.push(`for (let ${gen(s.iterator)} of ${gen(s.collection)}) {`);
       gen(s.body);
       output.push("}");
     },
-    // Conditional(e) {
-    //   return `((${gen(e.test)}) ? (${gen(e.consequent)}) : (${gen(
-    //     e.alternate
-    //   )}))`;
-    // },
     BinaryExpression(e) {
       const op = { "==": "===", "!=": "!==" }[e.op] ?? e.op;
       return `(${gen(e.left)} ${op} ${gen(e.right)})`;
@@ -147,37 +74,19 @@ export default function generate(program) {
       return `${e.op}(${operand})`;
     },
 
-    // ArrayExpression(e) {
-    //   return `[${gen(e.elements).join(",")}]`;
-    // },
-    // EmptyArray(e) {
-    //   return "[]";
-    // },
     MemberExpression(e) {
       const object = gen(e.object);
       const field = JSON.stringify(gen(e.field));
       const chain = e.op === "." ? "" : e.op;
       return `(${object}${chain}[${field}])`;
     },
-    // FunctionCall(c) {
-    //   const targetCode = standardFunctions.has(c.callee)
-    //     ? standardFunctions.get(c.callee)(gen(c.args))
-    //     : `${gen(c.callee)}(${gen(c.args).join(", ")})`;
-    //   // Calls in expressions vs in statements are handled differently
-    //   if (c.callee.type.returnType !== Type.VOID) {
-    //     return targetCode;
-    //   }
-    //   output.push(`${targetCode};`);
-    // },
     ConstructorCall(c) {
       return `new ${gen(c.callee)}(${gen(c.args).join(", ")})`;
     },
     Number(e) {
       return e;
     },
-    // Boolean(e) {
-    //   return e;
-    // },
+
     StringLiteral(e) {
       return e;
     },
